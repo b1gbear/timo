9f88f3dbe9f128c1e1370f2004904f46755c5e3a
Your Name <you@example.com>
2021-03-15 23:26:16 +0100

    Improve row and columns

--- src/DataForm.js
+++ src/DataForm.js
@@ -10,6 +10,91 @@ class DataForm extends React.Component {
         }
     }
 
+    change = e => {
+        console.log({
+            [e.target.name]: e.target.value
+        })
+
+        const res = e.target.name.split("_");
+        console.log(res)
+
+        const name = res[0]
+        if ("coeff" === name) {
+            const rowIndex = parseInt(res[1])
+            const colIndex = parseInt(res[2])
+            console.log("chan cofe22")
+            const num = parseFloat(e.target.value.replace(",", "."))
+            if (!isNaN(num)) {
+                this.setState(state => {
+                    return state.list[rowIndex][colIndex] = num
+                })
+            }
+
+        } else if ("c" === "name") {
+            console.log("chan sdf2dcofe")
+
+            const rowIndex = parseInt(res[1])
+            const num = parseFloat(e.target.value.replace(",", "."))
+
+            if (!isNaN(num)) {
+                this.setState(state => {
+                    return state.c[rowIndex] = num
+                })
+            }
+
+        }
+
+    }
+
+    formChange = e => {
+        const name = e.target.name
+        console.log(`Action is ${name}`)
+        switch (name) {
+            case 'row_inc':
+                this.setState(state => {
+
+                    if (state.list.length === 0) {
+                        state.list.push([])
+                    }
+                    {
+                        state.list.push(Array(state.list[0].length).fill(0))
+                    }
+                    state.c.push(0)
+                    return state
+                })
+                break;
+            case 'row_dec':
+                this.setState(state => {
+                    if (state.list.length !== 0) {
+                        state.list.pop()
+                        state.c.pop()
+                    }
+                    return state
+                })
+                break;
+            case 'col_inc':
+                this.setState(state => {
+                    state.list.forEach(row => {
+                        row.push(0)
+                    })
+                    return state
+                })
+                break;
+            case 'col_dec':
+                this.setState(state => {
+                    state.list.forEach(row => {
+                        if (row.length !== 0) {
+                            row.pop()
+                        }
+                    })
+                    return state
+                })
+                break;
+            default:
+                console.error(`Unknown argument ${name}`);
+        }
+
+    }
 
     render() {
         return (
@@ -17,20 +102,27 @@ class DataForm extends React.Component {
                 <div id={"box"}>
                     <div id={"a"}>
                         <form>
-                            <table style={{"width":"100%"}}>
+                            <table style={{"width": "100%"}}>
                                 {
                                     this.state.list.map((value, index) => {
                                         const list = value.map((innerValue, innerIndex) => {
                                             return <td key={index + "_" + innerIndex}>
                                                 <input
-                                                    key={"input_" + index + "_" + innerIndex}
+                                                    name={"coeff_" + index + "_" + innerIndex}
                                                     value={this.state.list[index][innerIndex]}
+                                                    onChange={e => this.change(e)}
+                                                    type="number" step="any"
                                                 />
                                             </td>
                                         })
                                         return <tr>
                                             {list}
-                                            <input key={"input_c_" + index} value={this.state.c[index]}/>
+                                            <input
+                                                name={"c_" + index}
+                                                value={this.state.c[index]}
+                                                onChange={e => this.change(e)}
+                                                type="number" step="0.0001"
+                                            />
                                         </tr>
                                     })
                                 }
@@ -38,12 +130,16 @@ class DataForm extends React.Component {
                         </form>
                     </div>
                     <div id={"b"}>
-                        <div><Button>Dodaj kolumnę</Button></div>
-                        <div><Button>Usuń kolumnę</Button></div>
+                        <div>
+                            <Button name={"col_inc"} onClick={e => this.formChange(e)}>Dodaj kolumnę</Button>
+                        </div>
+                        <div>
+                            <Button name={"col_dec"} onClick={e => this.formChange(e)}>Usuń kolumnę</Button>
+                        </div>
                     </div>
                 </div>
-                <Button>Dodaj wiersz</Button>
-                <Button>Usuń wiersz</Button>
+                <Button name={"row_inc"} onClick={e => this.formChange(e)}>Dodaj wiersz</Button>
+                <Button name={"row_dec"} onClick={e => this.formChange(e)}>Usuń wiersz</Button>
             </div>
         )
     }
