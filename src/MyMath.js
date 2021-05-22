48f907ed13a67d6acfc25425e7c0dd829ab1f0af
Your Name <you@example.com>
2021-05-22 15:49:55 +0200

    Basic gauss elimination algorithm

--- src/MyMath.js
+++ src/MyMath.js
@@ -1,3 +1,14 @@
+class MyMathError extends Error {
+    constructor(foo = 'bar', ...params) {
+        // Pass remaining arguments (including vendor specific ones) to parent constructor
+        super(...params);
+        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, CustomError);
+        }
+    }
+}
+
 class MyMath {
     isInt = n => {
         return Number(n) === n && n % 1 === 0;
@@ -11,7 +22,7 @@ class MyMath {
     }
 
     isNumericVector = v => {
-        if (!this.isObject(v)){
+        if (!this.isObject(v)) {
             return false
         }
         if (!Array.isArray(v)) {
@@ -40,19 +51,18 @@ class MyMath {
             }
 
 
-            for ( let j = 0 ; j < matrix[i].length; j ++) {
+            for (let j = 0; j < matrix[i].length; j++) {
                 const value = matrix[i][j]
-                if (!(this.isInt(value) || this.isFloat(value)))
-                {
+                if (!(this.isInt(value) || this.isFloat(value))) {
                     return null
                 }
             }
 
         }
-        if (firstLength == null){
+        if (firstLength == null) {
             return null
         }
-        return [matrix.length,firstLength]
+        return [matrix.length, firstLength]
     }
 
     isConsistent2dMatirx = m => {
@@ -77,6 +87,94 @@ class MyMath {
         })
         return matrix
     }
+
+
+    ge = (A, B) => {
+        const isize = A.length // Number of rows
+
+        const NO_SOLUTION = {"A": null, "B": null, "C": false};
+        for (let outerRowIterator = 0; outerRowIterator < A.length; outerRowIterator++) {
+
+            // Find non zero row
+            let found = this.findNonZeroRowInMatrix(A, outerRowIterator)
+
+            if (found < 0) {
+                return NO_SOLUTION
+            }
+
+            // Swap rows
+            this.swapRow(A, found, outerRowIterator)
+            this.swapRow(B, found, outerRowIterator)
+
+            // Subtract row pointed by outerRowIterator multiplied by factor
+            // from row at innerRow position.
+            for (let innerRow = outerRowIterator + 1; innerRow < isize; innerRow++) {
+                const factor = A[innerRow][outerRowIterator] / A[outerRowIterator][outerRowIterator];
+                A[innerRow] = this.addVectorToVector(A[innerRow], this.multiplyVectorByConstant(-factor, A[outerRowIterator]))
+                B[innerRow] = B[innerRow] - factor * B[outerRowIterator];
+            }
+        }
+
+
+        //
+        // console.error("A", A)
+        // console.error("B", B)
+        // Iterate upwards from last index to create eye matrix for A
+        // and results for B,
+        // then multiplication of A*X = X and X = B that means we found solution
+        for (let i = isize - 1; i >= 0; i--) {
+
+            // Divide right side by A so value of X(i) is known
+            B[i] = B[i] / A[i][i];
+
+            // Zeroing whole column above and subtracting from B vector
+            for (let j = i - 1; j >= 0; j--) {
+                // Moving solved variable to right side
+                B[j] = B[j] - A[j][i] * B[i]
+                // Zeroing row
+                A[j][i] = 0
+            }
+            // Creating eye matrix
+
+            A[i][i] = 1
+        }
+
+        return {"A": A, "B": B, "C": true, "reason" : null}
+    }
+
+
+    // Swap rows i and j and return swapped matrix
+    swapRow = (X, i, j) => {
+        const temp = X[i]
+        X[i] = X[j]
+        X[j] = temp
+    }
+
+
+    multiplyVectorByConstant = (constant, vec) => {
+        return vec.map(function (x) {
+            return x * constant;
+        });
+    }
+
+    addVectorToVector = (a, b) => {
+        let c = []
+        a.forEach((item, index) => {
+            c.push(item + b[index]);
+        })
+        return c
+    }
+
+
+    findNonZeroRowInMatrix = (matrix, start) => {
+        let found = -1
+        for (let i = start; i < matrix.length; i++) {
+            if (matrix[i][start] !== 0)
+                return i
+        }
+        return found
+    }
+
 
 }
 
--- src/MyMath.test.js
+++ src/MyMath.test.js
@@ -40,26 +40,26 @@ test('getConsistentMatrixSize returns true only for vector', () => {
         [1.0, 1.2, 1.3]
     )).toBeNull();
     expect((new MyMath()).getConsistentMatrixSize(
-        [[1.0, 1.2, 1.3],[1.0, 2.0]]
+        [[1.0, 1.2, 1.3], [1.0, 2.0]]
     )).toBeNull();
     expect((new MyMath()).getConsistentMatrixSize(
         [
             [1.0, 1.2, 1.3],
-            [1.0, 2.0,3.0]
+            [1.0, 2.0, 3.0]
         ]
-    )).toStrictEqual([2,3]);
+    )).toStrictEqual([2, 3]);
     expect((new MyMath()).getConsistentMatrixSize(
         [
             [1.0, 1.2, 1.3],
-            [1.0, 2.0,3.0],
-            [1.0, 2.0,3.1]
+            [1.0, 2.0, 3.0],
+            [1.0, 2.0, 3.1]
         ]
-    )).toStrictEqual([3,3]);
+    )).toStrictEqual([3, 3]);
     expect((new MyMath()).getConsistentMatrixSize(
         [
             [1.0, 1.2, 1.3],
-            [1.0, 2.0,3.0],
-            [1.0, 2.0,'x']
+            [1.0, 2.0, 3.0],
+            [1.0, 2.0, 'x']
         ]
     )).toBeNull();
 });
@@ -70,51 +70,112 @@ test('getConsistentMatrixSize returns true only for vector', () => {
         [1.0, 1.2, 1.3]
     )).toBeFalsy();
     expect((new MyMath()).isConsistent2dMatirx(
-        [[1.0, 1.2, 1.3],[1.0, 2.0]]
+        [[1.0, 1.2, 1.3], [1.0, 2.0]]
     )).toBeFalsy();
     expect((new MyMath()).isConsistent2dMatirx(
         [
             [1.0, 1.2, 1.3],
-            [1.0, 2.0,3.0]
+            [1.0, 2.0, 3.0]
         ]
     )).toBeTruthy();
     expect((new MyMath()).isConsistent2dMatirx(
         [
             [1.0, 1.2, 1.3],
-            [1.0, 2.0,3.0],
-            [1.0, 2.0,3.1]
+            [1.0, 2.0, 3.0],
+            [1.0, 2.0, 3.1]
         ]
     )).toBeTruthy();
     expect((new MyMath()).isConsistent2dMatirx(
         [
             [1.0, 1.2, 1.3],
-            [1.0, 2.0,3.0],
-            [1.0, 2.0,'x']
+            [1.0, 2.0, 3.0],
+            [1.0, 2.0, 'x']
         ]
     )).toBeFalsy();
 });
 
 
 test('create2dMatrix creates matrices with desired shape', () => {
-    const matrix10x3 = (new MyMath()).create2dMatrix(10,3)
+    const matrix10x3 = (new MyMath()).create2dMatrix(10, 3)
     const matrixSize = (new MyMath()).getConsistentMatrixSize(matrix10x3)
-    expect(matrixSize).toStrictEqual([10,3]);
+    expect(matrixSize).toStrictEqual([10, 3]);
 });
 
 
-
 test('create2dMatrix creates matrices with desired shape', () => {
-    const vector = [1.0,20.0,13.0]
+    const vector = [1.0, 20.0, 13.0]
     const matrix3x1 = (new MyMath()).numericVectorToMatrix(vector)
     const matrixSize = (new MyMath()).getConsistentMatrixSize(matrix3x1)
-    expect(matrixSize).toStrictEqual([3,1]);
+    expect(matrixSize).toStrictEqual([3, 1]);
     expect(matrix3x1[0][0]).toStrictEqual(1.0);
     expect(matrix3x1[1][0]).toStrictEqual(20.0);
     expect(matrix3x1[2][0]).toStrictEqual(13.0);
 });
 
+// Gauss-Elimination part
+
+test('swapRow', () => {
+    let r = [1, 2, 3];
+    (new MyMath()).swapRow(r, 0, 1)
+    expect(r).toStrictEqual([2, 1, 3]);
+});
+
+test('multiplyVectorByConstant', () => {
+    let r = [1, 2, 3];
+    let c = (new MyMath()).multiplyVectorByConstant(3, r)
+    expect(r).toStrictEqual([1, 2, 3]);
+    expect(c).toStrictEqual([3, 6, 9]);
+});
+
+test('addVectorToVector', () => {
+    let a = [1, 2, 3];
+    let b = [33, 12, 23];
+
+    const c = (new MyMath()).addVectorToVector(a, b)
+    expect(a).toStrictEqual([1, 2, 3]);
+    expect(b).toStrictEqual([33, 12, 23]);
+    expect(c).toStrictEqual([34, 14, 26]);
+});
+
+test('findNonZeroRowInMatrix', () => {
+    let A = [
+        [0, 2, 3, 2],
+        [0, 4, 0, 5],
+        [2, 0, 0, 0],
+        [1, 5, 6, 8]
+    ];
+
+    expect((new MyMath()).findNonZeroRowInMatrix(A, 0)).toStrictEqual(2);
+    expect((new MyMath()).findNonZeroRowInMatrix(A, 1)).toStrictEqual(1);
+    expect((new MyMath()).findNonZeroRowInMatrix(A, 2)).toStrictEqual(3);
+    expect((new MyMath()).findNonZeroRowInMatrix(A, 3)).toStrictEqual(3);
+
+});
+
+
+test('ge1', () => {
+    let A = [[3, 1, -1, 1], [1, 5, -3, 0], [1, 3, 0, -1], [0, 3, 2, 1]]
+    let B = [0, 7, 1, 2]
+    let C = [-1, 1, -1, 1]
+
+    let delta = 1e-5
+    const R = (new MyMath()).ge(A, B).B
+    for (let i = 0; i < R.length; i++) {
+        expect(Math.abs(R[i] - C[i]) < delta).toBeTruthy()
+    }
+});
 
 
 
+test('ge4', () => {
+    let A = [[-1, 2, 1], [1, -3, -2], [3, -1, -1]]
+    let B = [-1, -1, 4]
+    let C = [2, -1, 3]
 
+    let delta = 1e-5
+    const R = (new MyMath()).ge(A, B).B
+    for (let i = 0; i < R.length; i++) {
+        expect(Math.abs(R[i] - C[i]) < delta).toBeTruthy()
+    }
+});
 
