c32db84d5e4509f29f74dbf658b8d06be2de0417
Your Name <you@example.com>
2021-05-23 14:12:51 +0200

    Add non tested one phase simplex

---
+++ src/OnePhaseSimplex.js
@@ -0,0 +1,200 @@
+/*
+Krok 1.to rozpoczęcie obliczeń i napisanie tablicy simpleksowej oraz sprawdzić czy wszystkie wartości w 1 kolumnie są ≥0,
+ czyli czy można rozwiązać simpleksem prymalnym, jeśli nie to simpleks prymalny sobie nie poradzi i trzeba będzie
+ wykorzystać simpleks dwufazowy.
+2.Krok 2. to test optymalności, który polega na sprawdzeniu co się dzieje w wartościach dla funkcji celu (jeżeli (x1x2)
+sąsame wartości dodatnie to zmienne (x3,x4,x5) są rozwiązaniem optymalnym. Jeśli nie to przechodzimy do kroku 3.
+3.Krok 3. Wybór zmiennej wchodzącej do bazy, tę która ma największą wartość ujemną.
+4.Krok 4. To wybór zmiennej, którą chcemy usunąć z bazy. Liczymy ilorazy (np. 5/1, nie liczymy bo ograniczenie jest
+  rozwarte, 21/6)
+5.Krok 5. Jest to eliminacja Gauss’a. Wyliczamy nową tablicę simpleksową. Wiersz gdzie jest element centralny jest to
+  iloraz przez element centralny(21/6; 2/6), a w miejscu gdzie był element centralny jest jego odwrotność (1/6).
+
+
+ */
+import MyMath from "./MyMath";
+import * as Utils from "./MyUtils";
+
+function assert(condition, message) {
+    if (!condition) {
+        throw message || "Assertion failed";
+    }
+}
+
+
+class OnePhaseSimplex {
+    math = new MyMath()
+
+    constructSimplexArray = (
+        c_n,
+        b,
+        N
+    ) => {
+        /*
+         * Create simplex array referring to table below
+         *
+         * +------+---+------+
+         * |      |   | -x_n |
+         * +------+---+------+
+         * | x_0  | 0 | c_n  |
+         * | x_b  | b | N    |
+         * +------+---+------+
+         *
+         * name     dim    explained               ( type )
+         *    b  : [s,1] : constraints wyraz wolny ( scalars )
+         *  x_0  : [1,1] : function celu           ( scalar )
+         *  x_b  : [s,1] : base variables vector   ( symbols )
+         * -x_n  : [1,r] : minus non base x vector ( symbols )
+         *    N  : [s,r] : Non base vector         ( lefts part of A matrix )
+         *
+         *  s : number of constraints
+         *  r : number of non-artificial x variables
+         *
+         */
+        assert(this.math.isNumericVector(c_n), "c_n assertion")
+        assert(this.math.isNumericVector(b), "b assertion")
+        assert(this.math.isConsistent2dMatirx(N), "N assertion")
+        const s = b.length
+        const r = c_n.length
+        const matrix = this.math.create2dMatrix(s + 1, r + 1)
+        matrix[0][0] = 0.0
+        b.forEach((element, index) => {
+            matrix[1 + index][0] = element
+        })
+        c_n.forEach((element, index) => {
+            matrix[0][1 + index] = element
+        })
+        N.forEach((rowElement, rowIndex) => {
+            rowElement.forEach(
+                (element, index) => {
+                    matrix[rowIndex + 1][index + 1] = element
+                }
+            )
+        })
+        return matrix
+    }
+
+
+    wrapSimplexArrayAdditionalInfo = (
+        c_n,
+        b,
+        N
+    ) => {
+        return {
+            "table": this.constructSimplexArray(c_n, b, N),
+            "top": Utils.range(1, c_n.length + 1),
+            "left": [...Utils.range(c_n.length + 1, c_n.length + 1 + b.length)]
+        }
+    }
+
+
+    allCoefficientsInFirstColumnAboveOrEqualZero = simplexTable => {
+        for (let i = 0; i < simplexTable.length; i++) {
+            if (simplexTable[i][0] < 0)
+                return false
+        }
+        return true
+    }
+
+    allCoefficientsInFirstRowAboveOrEqualZero = simplexTable => {
+        for (let i = 0; i < simplexTable[0].length; i++) {
+            if (simplexTable[0][i] < 0)
+                return false
+        }
+        return true
+    }
+
+
+    minimalElementInFirstRowWhichIndicatesPivotColumn = (simplexTable) => {
+
+        let minIndex = null
+        for ( let i = 1 ; i < simplexTable[0].length; i++) {
+            if ( minIndex === null || simplexTable[0][i] < simplexTable[0][minIndex] ) {
+                minIndex = i
+            }
+        }
+
+        return minIndex
+    }
+
+    minimalRowBasedOnBCoefficientToPivotColumnElement = (simplexTable,pivotColumn) => {
+        let minIndex = null
+        let minFactor = null
+        for ( let row = 1; row < simplexTable.length ; row ++ ){
+            const factor = simplexTable[row][0] / simplexTable[row][pivotColumn]
+            if ( minIndex == null || factor < minFactor){
+                minIndex = row
+                minFactor = factor
+            }
+        }
+        return minIndex
+    }
+
+
+    // Swap rows i and j and return swapped matrix
+    swapBaseSymbols = (X, X_value, Y, Y_value) => {
+        const temp = X[X_value]
+        X[X_value] = Y[Y_value]
+        Y[Y_value] = temp
+    }
+
+
+    primalSimplexIteration = fullSimplexTable => {
+        const simplexTable = fullSimplexTable.table
+        const result = this.math.getConsistentMatrixSize(simplexTable)
+        const rows = result[0]
+        const cols = result[1]
+        const minCol = this.minimalElementInFirstRowWhichIndicatesPivotColumn(simplexTable)
+        const minRow = this.minimalRowBasedOnBCoefficientToPivotColumnElement(simplexTable,minCol)
+        this.gaussian_elimination(simplexTable,minRow,minCol,rows,cols)
+        this.swapBaseSymbols(fullSimplexTable.top,minCol-1,fullSimplexTable.left,minRow-1)
+    }
+
+    onePhaseSimplex = (
+        c_n,
+        b,
+        N
+    ) => {
+        let fullSimplexTable = this.wrapSimplexArrayAdditionalInfo(c_n, b, N)
+        let i = 0
+        while (!this.allCoefficientsInFirstRowAboveOrEqualZero(fullSimplexTable.table)) {
+            this.primalSimplexIteration(fullSimplexTable)
+            console.log(i)
+        }
+
+        return this.simplexResult(fullSimplexTable)
+    }
+
+
+
+    gaussian_elimination = (a, exact_row, exact_col, rows, cols) => {
+        const b = Utils.copy(a)
+        Utils.range(rows).forEach((i) => {
+            Utils.range(cols).forEach((j) => {
+                if (i === exact_row && j === exact_col) {
+                    a[i][j] = 1 / b[i][j]
+                } else if (i === exact_row && j !== exact_col) {
+                    a[i][j] = b[exact_row][j] / b[exact_row][exact_col]
+                } else if (i !== exact_row && j === exact_col) {
+                    a[i][j] = -b[i][exact_col] / b[exact_row][exact_col]
+                } else /* any other element not in (col or row) */{
+                    a[i][j] = b[i][j] - b[i][exact_col] * b[exact_row][j] / b[exact_row][exact_col]
+                }
+            })
+        })
+        return a
+    }
+
+    simplexResult = (fullSimplexTable) => {
+        let result = new Array(fullSimplexTable.top.length+fullSimplexTable.left.length+1)
+        result = result.fill(0)
+        result[0] = fullSimplexTable.table[0][0]
+        for (let i = 0 ; i <  fullSimplexTable.left.length ; i ++ ){
+            const element = fullSimplexTable.left[i]
+            result[element] = fullSimplexTable.table[i+1][0]
+        }
+        return result;
+    }
+}
+
+export default OnePhaseSimplex
---
+++ src/OnePhaseSimplex.test.js
@@ -0,0 +1,201 @@
+import OnePhaseSimplex from "./OnePhaseSimplex";
+import * as Utils from "./MyUtils";
+
+
+test('Two phase simplex constructSimplexArray', () => {
+    const c_n = [1.0, 2.0, 3.0, 4.0]
+    const b = [5.0, 6.0, 7.0]
+    const N = [
+        [8.0, 9.0, 10.0, 11.0],
+        [12.0, 13.0, 14.0, 15.0],
+        [16.0, 17.0, 18.0, 19.0]
+    ]
+
+    const have = (new OnePhaseSimplex()).constructSimplexArray(c_n, b, N)
+
+    const expected =
+        [
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 8.0, 9.0, 10.0, 11.0],
+            [6.0, 12.0, 13.0, 14.0, 15.0],
+            [7.0, 16.0, 17.0, 18.0, 19.0]
+        ]
+    expect(have).toStrictEqual(expected)
+});
+
+
+test('allCoefficientsInFirstRowAboveOrEqualZero', () => {
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstColumnAboveOrEqualZero([
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 1.0, 9.0, 0.0, 1.0],
+            [6.0, 1.0, 3.0, 4.0, 5.0],
+            [7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeTruthy()
+
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstColumnAboveOrEqualZero([
+            [-1.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 1.0, 9.0, 0.0, 1.0],
+            [6.0, 1.0, 3.0, 4.0, 5.0],
+            [7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeFalsy()
+
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstColumnAboveOrEqualZero([
+            [-1.0, 1.0, 2.0, 3.0, 4.0],
+            [-5.0, 1.0, 9.0, 0.0, 1.0],
+            [-6.0, 1.0, 3.0, 4.0, 5.0],
+            [-7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeFalsy()
+});
+
+
+test('allCoefficientsInFirstRowAboveOrEqualZero', () => {
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstRowAboveOrEqualZero([
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 1.0, 9.0, 0.0, 1.0],
+            [6.0, 1.0, 3.0, 4.0, 5.0],
+            [7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeTruthy()
+
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstRowAboveOrEqualZero([
+            [-1.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 1.0, 9.0, 0.0, 1.0],
+            [6.0, 1.0, 3.0, 4.0, 5.0],
+            [7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeFalsy()
+
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstRowAboveOrEqualZero([
+            [-1.0, -1.0, -2.0, -3.0, -4.0],
+            [-5.0, 1.0, 9.0, 0.0, 1.0],
+            [-6.0, 1.0, 3.0, 4.0, 5.0],
+            [-7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeFalsy()
+
+    expect(
+        (new OnePhaseSimplex()).allCoefficientsInFirstRowAboveOrEqualZero([
+            [0.0, 0.0, 0.0, 0.0, 0.0],
+            [-5.0, 1.0, 9.0, 0.0, 1.0],
+            [-6.0, 1.0, 3.0, 4.0, 5.0],
+            [-7.0, 1.0, 7.0, 8.0, 9.0]
+        ])
+    ).toBeTruthy()
+});
+
+
+test('wrapSimplexArrayAdditionalInfo', () => {
+
+    const c_n = [1.0, 2.0, 3.0, 4.0]
+    const b = [5.0, 6.0, 7.0]
+    const N = [
+        [8.0, 9.0, 10.0, 11.0],
+        [12.0, 13.0, 14.0, 15.0],
+        [16.0, 17.0, 18.0, 19.0]
+    ]
+
+    const have = (new OnePhaseSimplex()).wrapSimplexArrayAdditionalInfo(c_n, b, N)
+    expect(have.table).toStrictEqual([
+        [0.0, 1.0, 2.0, 3.0, 4.0],
+        [5.0, 8.0, 9.0, 10.0, 11.0],
+        [6.0, 12.0, 13.0, 14.0, 15.0],
+        [7.0, 16.0, 17.0, 18.0, 19.0]
+    ])
+    expect(have.left).toStrictEqual([5,6,7])
+    expect(have.top).toStrictEqual([1, 2, 3,4])
+})
+
+
+test('minimalRowBasedOnBCoefficientToPivotColumnElement', () => {
+    const simplexTable = [
+        [0.0, 3.0, 2.0, 1.0, 3.0],
+        [7.0, 8.0, 9.0, 18.0, 11.0],
+        [6.0, 12.0, 13.0, 1.0, 15.0],
+        [6.0, 16.0, 17.0, 14.0, 19.0]
+    ]
+    const minCol = (new OnePhaseSimplex()).minimalElementInFirstRowWhichIndicatesPivotColumn(simplexTable)
+    expect(
+        minCol
+    ).toStrictEqual(3)
+
+    expect(
+        (new OnePhaseSimplex()).minimalRowBasedOnBCoefficientToPivotColumnElement(simplexTable, minCol)
+    ).toStrictEqual(1)
+})
+
+test('swapBaseSymbols', () => {
+
+    const X = [1, 2, 3];
+    const Y = [4, 9, 16];
+
+
+    (new OnePhaseSimplex()).swapBaseSymbols(X, 2, Y, 1)
+    expect(X).toStrictEqual([1, 2, 9])
+    expect(Y).toStrictEqual([4, 3, 16])
+
+})
+
+
+test('gaussian_elimination', () => {
+
+    //
+    // (new OnePhaseSimplex()).gaussian_elimination(X, 2, Y, 1)
+    // expect(X).toStrictEqual([1, 2, 9])
+    // expect(Y).toStrictEqual([4, 3, 16])
+
+})
+
+
+test('simplexResult', () => {
+
+    const simplex_mock = {
+        "table": [
+            [3.0, 3.0, 2.0, 1.0, 3.0],
+            [7.0, 8.0, 9.0, 18.0, 11.0],
+            [6.0, 12.0, 13.0, 1.0, 15.0],
+            [9.0, 16.0, 17.0, 14.0, 19.0]
+        ],
+        "top": [3,4,5,6],
+        "left": [1, 2, 5]
+    }
+
+
+    expect((new OnePhaseSimplex()).simplexResult(simplex_mock))
+        .toStrictEqual([3.0, 7.0, 6.0, 0.0, 0.0, 9.0,0.0,0.0])
+
+
+})
+
+
+test('onePhaseSimplex', () => {
+
+    const N = [
+        [1, 1],
+        [-1, 1],
+        [ 6, 2]
+    ]
+    const b = [5, 0, 21]
+
+    const c_n = [-2, -1]
+
+
+    expect((new OnePhaseSimplex()).onePhaseSimplex(c_n, b,N))
+        .toStrictEqual([31/4,11/4,9/4,0,1/2,0])
+
+
+})
+
+
+
+// primalSimplexIteration
+// onePhaseSimplex
+// simplexResult
