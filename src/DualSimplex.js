c4fbe06da97695661c08de1713566de5fc835683
Your Name <you@example.com>
2021-05-23 15:24:12 +0200

    Add tests

--- src/DualSimplex.js
+++ src/DualSimplex.js
@@ -1,8 +1,61 @@
 import MyMath from "./MyMath";
 import * as Utils from "./MyUtils";
+import OnePhaseSimplex from "./OnePhaseSimplex";
 
 
 class DualSimplex {
+    onePhase = new OnePhaseSimplex()
+
+    allCoefficientsInFirstColumnAboveOrEqualZero(simplexTable) {
+        for (let i = 1 ; i < simplexTable[0].length; i ++){
+            if (simplexTable[i][0] < 0){
+                return false
+            }
+        }
+        return true
+    }
+
+    minimalNegativeElementInFirstColumn = (simplexTable) => {
+        let minI = null;
+        for( let i = 1 ; i < simplexTable.length ; i ++){
+            const value = simplexTable[i][0]
+            if ( ( value < 0 ) && ( minI == null || value < simplexTable[minI][0])   ){
+                minI = i
+            }
+        }
+        return minI
+    }
+
+    dualSimplexIteration = fullSimplexTable => {
+        const simplexTable = fullSimplexTable.table
+        const result = this.math.getConsistentMatrixSize(simplexTable)
+        const rows = result[0]
+        const cols = result[1]
+        const minCol = this.minimalNegativeElementInFirstColumn(simplexTable)
+        const minRow = this.minimalRowBasedOnBCoefficientToPivotColumnElement(simplexTable,minCol)
+        this.gaussian_elimination(simplexTable,minRow,minCol,rows,cols)
+        this.swapBaseSymbols(fullSimplexTable.top,minCol-1,fullSimplexTable.left,minRow-1)
+    }
+
+
+    onePhaseSimplex = (
+        c_n,
+        b,
+        N
+    ) => {
+        let fullSimplexTable = this.onePhase.wrapSimplexArrayAdditionalInfo(c_n, b, N)
+
+        if (!this.onePhase.allCoefficientsInFirstRowAboveOrEqualZero(fullSimplexTable.table)){
+            return null
+        }
+
+        while (!this.allCoefficientsInFirstColumnAboveOrEqualZero(fullSimplexTable.table)) {
+            this.dualSimplexIteration(fullSimplexTable)
+        }
+
+        return this.onePhase.simplexResult(fullSimplexTable)
+    }
+
 
 }
 
--- src/DualSimplex.test.js
+++ src/DualSimplex.test.js
@@ -2,3 +2,39 @@ import DualSimplex from "./DualSimplex";
 import * as Utils from "./MyUtils";
 
 
+
+test('minimalNegativeElementInFirstColumn', () => {
+    const matrix = [
+        [0,1,2,3,4],
+        [22],
+        [1],
+        [-100],
+        [-50]
+    ]
+    expect((new DualSimplex()).minimalNegativeElementInFirstColumn(matrix))
+        .toStrictEqual(3)
+})
+
+
+test('allCoefficientsInFirstColumnAboveOrEqualZero', () => {
+    const matrix = [
+        [0,1,2,3,4],
+        [22],
+        [1],
+        [-100],
+        [-50]
+    ]
+    expect((new DualSimplex()).allCoefficientsInFirstColumnAboveOrEqualZero(matrix)).toBeFalsy()
+    const matrix2 = [
+        [0,1,2,3,4],
+        [22],
+        [1],
+        [100],
+        [50]
+    ]
+    expect((new DualSimplex()).allCoefficientsInFirstColumnAboveOrEqualZero(matrix2)).toBeTruthy()
+})
+
+
+
+
